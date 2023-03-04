97ec0bd5ff0f20235075d317906f41409919bdd7
Glitch (glitch-hello-website) <none>
2023-03-04 20:55:14 +0000

    🚕🐵 Checkpoint ./index.html:45369638/584:55436971/2 ./script.js:45369638/44:55429653/4138

--- script.js
+++ script.js
@@ -32,37 +32,31 @@ Make the "Click me!" button move when the visitor clicks it:
 //   f.onclick = () => { goto(f.dataset.file, f.dataset.line); };
 // });
 
-
 const form1 = document.querySelector("#Textbox");
-const input1 = document.querySelector('#input1');
+const input1 = document.querySelector("#input1");
 const list_el1 = document.querySelector("#Task");
 
-form1.addEventListener('submit', (e) => {
-        e.preventDefault();
+form1.addEventListener("submit", (e) => {
+  e.preventDefault();
 
-        const task1 = input1.value;
+  const task1 = input1.value;
 
-      
+  const task_el1 = document.createElement("div");
+  task_el1.classList.add("Task");
 
-        const task_el1 = document.createElement("div");
-        task_el1.classList.add("Task");
+  const task_content_el1 = document.createElement("div");
+  task_content_el1.classList.add("content");
 
-        const task_content_el1 = document.createElement("div");
-        task_content_el1.classList.add("content");
+  task_el1.appendChild(task_content_el1);
 
+  const task_input_el1 = document.createElement("input");
+  task_input_el1.classList.add("text");
+  task_input_el1.type = "text";
+  task_input_el1.value = task1;
+  task_input_el1.setAttribute("readonly", "readonly");
 
-        task_el1.appendChild(task_content_el1);
+  task_content_el1.appendChild(task_input_el1);
 
-        const task_input_el1 = document.createElement("input");
-        task_input_el1.classList.add("text");
-        task_input_el1.type = "text";
-        task_input_el1.value = task1;
-        task_input_el1.setAttribute("readonly", "readonly");
-
-        task_content_el1.appendChild(task_input_el1);
-
-        const task_actions_el1 = document.createElement("div");
-        task_actions_el1.classList.add("actions");
-
-      
-    })
+  const task_actions_el1 = document.createElement("div");
+  task_actions_el1.classList.add("actions");
+});
